af960f0200bc5f1599c7ff955787b2040221d600
wolfieboy09 <105564186+wolfieboy09@users.noreply.github.com>
2024-01-06 19:25:22 -0500

    aaa

--- scripts/card_system.js
+++ scripts/card_system.js
@@ -1,5 +1,18 @@
 const apps = document.getElementsByClassName('cards');
 const apiURL = '';
+const jsonResponseElement = document.getElementById('jsonResponse');
 
-const app_data = fetch('http://localhost:8000/data_test.json');
-document.createElement("p").textContent = JSON.stringify(app_data);
+function getApps() {
+    return fetch('http://localhost:8000/data_test.json') // temporary
+    .then(responce => {
+        if (!responce.ok) {
+            throw new Error('Failed to fetch (Network not okay)');
+        }
+        return responce.json()
+    })
+    .catch(error => {
+        console.error('Problem with fetch operation: ', error)
+    });
+}
+
+jsonResponseElement.textContent = JSON.stringify(getApps(), null, 2);
