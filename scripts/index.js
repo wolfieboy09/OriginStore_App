f773c3b587c844817e6ab8c90ec87ea36fcb54e2
wolfieboy09 <105564186+wolfieboy09@users.noreply.github.com>
2024-01-07 19:12:09 -0500

    hi

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,40 @@
 const create_account_bttn = document.getElementById("make_account");
 const login_bttn = document.getElementById("login");
 
+
 create_account_bttn.addEventListener('click', () => {
     window.location.href = "create_account.html";
 });
+
+
+const app_card = document.querySelector('.cards');
+
+function getApps() {
+    return fetch('http://localhost:8000/data_test.json')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Failed to fetch (Network not okay)');
+            }
+            return response.json();
+        })
+        .catch(error => {
+            console.error('Problem with fetch operation: ', error);
+        });
+}
+
+getApps()
+    .then(data => {
+        data['apps'].forEach(item => {
+            const newDiv = document.createElement('div');
+            newDiv.className = item.name;
+            newDiv.id = 'app_card';
+            newDiv.innerHTML = `
+                <h3 id="${item.name}.name">${item.name}</h3>
+            `;
+            app_card.appendChild(newDiv);
+        });
+    })
+    .catch(error => {
+        console.error('Error in data retrieval: ', error);
+    });
+        
--- scripts/upload.js
+++
@@ -1,1 +0,0 @@
-const upload_url = "";
