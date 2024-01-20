23311350ba68bf4bbd9a1098a0da43c09c76ff4c
Mmm <112756430+koo1140@users.noreply.github.com>
2024-01-20 20:22:20 +0200

    Update background.js

--- scripts/background.js
+++ scripts/background.js
@@ -64,7 +64,7 @@ function animate() {
     drawPoints();
     setTimeout(function() {
         requestAnimationFrame(animate);
-    }, 10); // Increase or decrease the delay (in milliseconds) to control the speed
+    }, 50); // Increase or decrease the delay (in milliseconds) to control the speed
 }
 
 resize();
